52869fb3e5694847dc3c4ec162b7a3c52f279f89
kenanc66 <kenancicekdev@gmail.com>
2021-04-19 18:30:54 +0300

    arranged

--- frontend/src/app/components/product-add/product-add.component.ts
+++ frontend/src/app/components/product-add/product-add.component.ts
@@ -33,10 +33,10 @@ export class ProductAddComponent implements OnInit {
       let productModel = Object.assign({},this.productAddForm.value)
       this.productService.add(productModel).subscribe(response=>{
         this.toastrService.success(response.message,"Success")
-      },responseError=>{
-        if(responseError.error.Errors.length>0){
-          for (let i = 0; i <responseError.error.Errors.length; i++) {
-            this.toastrService.error(responseError.error.Errors[i].ErrorMessage
+      },response=>{
+        if(response.error.Errors.length>0){
+          for (let i = 0; i <response.error.Errors.length; i++) {
+            this.toastrService.error(response.error.Errors[i].ErrorMessage
               ,"Validation Error")
           }       
         } 
