d286846d171a63574d2d30b50c9b333c4a458fd5
kenanc66 <kenancicekdev@gmail.com>
2021-06-03 00:02:06 +0300

    product-add category ids arranged

--- frontend/src/app/components/product-add/product-add.component.ts
+++ frontend/src/app/components/product-add/product-add.component.ts
@@ -3,7 +3,8 @@ import {FormGroup,FormBuilder, FormControl, Validators} from "@angular/forms"
 import { ToastrService } from 'ngx-toastr';
 import { ProductService } from 'src/app/services/product.service';
 import {ErrorModel} from 'src/app/models/errorModel';
-
+import { Category } from 'src/app/models/category';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-product-add',
@@ -11,17 +12,19 @@ import {ErrorModel} from 'src/app/models/errorModel';
   styleUrls: ['./product-add.component.css']
 })
 export class ProductAddComponent implements OnInit {
- 
+  categories: Category[] = [];
   productAddForm : FormGroup;
   constructor(private formBuilder:FormBuilder, 
-    private productService:ProductService, private toastrService:ToastrService) { }
+    private productService:ProductService, private toastrService:ToastrService,private categoryService: CategoryService) { }
 
   ngOnInit(): void {
+    this.getCategories();
     this.createProductAddForm();
   }
 
   createProductAddForm(){
      this.productAddForm = this.formBuilder.group({
+      // ImageUrl:["",Validators.required],
       Name:["",Validators.required],
       categoryId:["",Validators.required],
       recipeExplanation:["", Validators.required],
@@ -30,10 +33,12 @@ export class ProductAddComponent implements OnInit {
        
      })
   }
-
+  
   add(){
     if(this.productAddForm.valid){
       let productModel = Object.assign({},this.productAddForm.value)
+      productModel.categoryId=Number(productModel.categoryId)
+      console.log("model",productModel)
       this.productService.add(productModel).subscribe(response=>{
         this.toastrService.success(response.message,"Success")
       },response=>{
@@ -51,6 +56,11 @@ export class ProductAddComponent implements OnInit {
       this.toastrService.error("Invalid Form Please Fill Correctly","Warning")
     }
     
+  }
+  getCategories() {
+    this.categoryService.getCategories().subscribe((response) => {
+      this.categories = response.data;
+    });
   }
 
 }
